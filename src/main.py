a6556ea7078585a1403aca0c6d4ce112a36ebc20
iyadhadife <ihadife@gmail.com>
2026-01-18 17:40:14 +0100

    Correction of some comment

--- src/main.py
+++ src/main.py
@@ -22,13 +22,13 @@ async def main():
     # 2. Initialisation du modèle local
     llm = ChatOllama(model=MODEL_NAME, temperature=0)
 
-    print(f"🌐 Tentative de connexion au serveur MCP...")
+    print(f"Tentative de connexion au serveur MCP...")
     
     try:
         async with sse_client(URL_MCP) as (read, write):
             async with ClientSession(read, write) as session:
                 await session.initialize()
-                print("✅ Connecté au RAG. Tapez 'exit' pour quitter.")
+                print("Connecté au RAG. Tapez 'exit' pour quitter.")
                 
                 while True:
                     user_input = input("\nQuestion > ")
@@ -42,7 +42,7 @@ async def main():
                     await generer_reponse_expert(session, llm, instructions, user_input)
 
     except Exception as e:
-        print(f"❌ Erreur de connexion au serveur MCP : {e}")
+        print(f"Erreur de connexion au serveur MCP : {e}")
         print("Vérifie que ton serveur MCP est bien lancé sur http://localhost:8000")
 
 if __name__ == "__main__":
